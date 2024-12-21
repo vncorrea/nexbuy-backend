ba94bfe6c31af110b8656a7770cbad215c2b0463
vncorrea <vinicorreag2001@gmail.com>
2024-12-21 13:24:07 -0300

    feat: importing routes in server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import connectDB from './config/db';
 import productRoutes from './routes/productRoutes';
+import cartRoutes from './routes/cartRoutes';
+import userRoutes from './routes/userRoutes';
+import checkoutRoutes from './routes/checkoutRoutes';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -12,6 +15,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/products', productRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/checkout', checkoutRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send({ message: 'API is running with TypeScript!' });

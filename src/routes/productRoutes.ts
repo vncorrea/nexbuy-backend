8309326a06409432254f512b42f8773cc0dfff01
vncorrea <vinicorreag2001@gmail.com>
2024-12-21 00:43:12 -0300

    fix: validate error

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -19,7 +19,11 @@ Router.post("/create", async (req, res) => {
         const createdProduct = await product.save();
         res.status(201).json(createdProduct);
     } catch (error) {
-        res.status(500).json({ message: "Server Error" });
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        } else {
+            res.status(500).json({ message: "An unknown error occurred" });
+        }
     }
 });
 
